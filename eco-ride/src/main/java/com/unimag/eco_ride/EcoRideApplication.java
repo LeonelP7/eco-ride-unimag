05ead48f8b50260459cac75b3e654a994d4be5f8
Saxyz <sarmientun@gmail.com>
2025-11-16 15:02:48 -0500

    First commit

---
+++ eco-ride/src/main/java/com/unimag/eco_ride/EcoRideApplication.java
@@ -0,0 +1,13 @@
+package com.unimag.eco_ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoRideApplication.class, args);
+	}
+
+}
