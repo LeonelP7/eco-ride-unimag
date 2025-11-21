a6b9f480f4ae4dc9b252a93e5e078dc303f5a18d
leonelp <pereiraleonel287@gmail.com>
2025-11-21 02:10:56 -0500

    Starting SAGA pattern with choreography, adding methods to the ReservationService

--- eco-ride/trip-service/src/main/java/com/unimag/trip_service/services/ReservationService.java
+++ eco-ride/trip-service/src/main/java/com/unimag/trip_service/services/ReservationService.java
@@ -2,6 +2,8 @@ package com.unimag.trip_service.services;
 
 import com.unimag.trip_service.dtos.reservation.CreateReservationDTO;
 import com.unimag.trip_service.dtos.reservation.ResponseReservationDTO;
+import com.unimag.trip_service.events.PaymentAuthorized;
+import com.unimag.trip_service.events.PaymentFailedEvent;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface ReservationService {
     ResponseReservationDTO registerReservation(CreateReservationDTO createReservationDTO);
     List<ResponseReservationDTO> findAll();
     ResponseReservationDTO findById(String id);
+    void processPaymentAuthorized(PaymentAuthorized event);
+    void processPaymentFailed(PaymentFailedEvent event);
 }
