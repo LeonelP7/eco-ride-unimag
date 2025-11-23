299240e2893b78ec4ae0e91d5939105c849757f6
leonelp <pereiraleonel287@gmail.com>
2025-11-22 23:48:36 -0500

    fixing api-gateway, and making everythin in the trip-service reactive, Flyway still needs to be implemented

--- eco-ride/trip-service/src/main/java/com/unimag/trip_service/events/PaymentAuthorizedEvent.java
+++ eco-ride/trip-service/src/main/java/com/unimag/trip_service/events/PaymentAuthorizedEvent.java
@@ -3,6 +3,8 @@ package com.unimag.trip_service.events;
 public record PaymentAuthorizedEvent(
         String reservationId,
         String paymentIntedId,
-        String chargeId
+        String chargeId,
+        String email,
+        String passengerName
 ) {
 }
--- eco-ride/trip-service/src/main/java/com/unimag/trip_service/respositories/ReservationRepository.java
+++ eco-ride/trip-service/src/main/java/com/unimag/trip_service/respositories/ReservationRepository.java
@@ -1,7 +1,7 @@
 package com.unimag.trip_service.respositories;
 
 import com.unimag.trip_service.entities.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation, String> {
+public interface ReservationRepository extends R2dbcRepository<Reservation, String> {
 }
